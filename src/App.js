e7c1ca348b2596f0b2ff7aea8c88d3d4574d3124
herdin27 <it06.knitto@gmail.com>
2023-03-02 17:00:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Cleave from 'cleave.js/react';
+import CreditCardDetector from './utils/creditCardDetector';
+import { useState } from 'react';
 
 function App() {
+
+  const [state, setState] = useState('')
+
+  const onCreditCardChange = (e) => {
+    let result
+    const creditCardInfo = CreditCardDetector.getInfo(e.target.value, 'strict mode');
+    const detector = creditCardInfo.type
+    result = detector
+    if (detector === 'amex') {
+      result = 'American Express'
+    }
+    if (detector === 'uatp') {
+      result = 'Universal Air Travel Plan'
+    }
+    if (detector === 'uatp') {
+      result = 'Universal Air Travel Plan'
+    }
+    setState(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Cleave placeholder="Enter your credit card number"
+          options={{ creditCard: true }}
+          onChange={onCreditCardChange} />
+        <button onClick={() => setState('')}>clear</button>
+      </div>
+      <h1>ini kartu {state === 'unknown' ? 'Tidak tedefinisi' : state}</h1>
+    </>
   );
 }
 
