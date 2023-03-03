a03eabf1b1a5e7a6aa367cf567f84a8301f026a5
herdin27 <it06.knitto@gmail.com>
2023-03-03 10:16:37 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Cleave from 'cleave.js/react';
 import CreditCardDetector from './utils/creditCardDetector';
 import { useState } from 'react';
 
 function App() {
 
   const [state, setState] = useState('')
+  const [value, setValue] = useState('')
 
   const onCreditCardChange = (e) => {
+    setState(e.target.value)
     let result
-    const creditCardInfo = CreditCardDetector.getInfo(e.target.value, 'strict mode');
+    const creditCardInfo = CreditCardDetector.getInfo(state, 'strict mode');
     const detector = creditCardInfo.type
+    console.log(state)
     result = detector
     if (detector === 'amex') {
       result = 'American Express'
@@ -20,18 +22,23 @@ function App() {
     if (detector === 'uatp') {
       result = 'Universal Air Travel Plan'
     }
-    setState(result)
+    setValue(result)
+  }
+  const handleClear = () => {
+    setState('')
+    setValue('')
   }
 
   return (
     <>
       <div>
-        <Cleave placeholder="Enter your credit card number"
-          options={{ creditCard: true }}
+        <input placeholder="Enter your credit card number"
+          name='credit'
+          value={state}
           onChange={onCreditCardChange} />
-        <button onClick={() => setState('')}>clear</button>
+        <button onClick={handleClear}>clear</button>
       </div>
-      <h1>ini kartu {state === 'unknown' ? 'Tidak tedefinisi' : state}</h1>
+      <h1>ini kartu {value === 'unknown' ? 'Tidak tedefinisi' : value}</h1>
     </>
   );
 }
