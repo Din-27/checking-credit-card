7ef5b51a78c75d8a5584038d543e5283769bb018
herdin27 <it06.knitto@gmail.com>
2023-04-17 13:57:10 +0700

    add credit Card type but using vanilla js

--- src/utils/vanilaJs.js
+++ src/utils/vanilaJs.js
@@ -1,12 +1,14 @@
 export const creditCardType = (cc) => {
+
+    let uatp = new RegExp('^(?!1800)1{0,14}')
     let amex = new RegExp('^3[47][0-9]{13}$');
     let visa = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
     let cup1 = new RegExp('^62[0-9]{14}[0-9]*$');
     let cup2 = new RegExp('^81[0-9]{14}[0-9]*$');
-
+    let dankort = new RegExp('^(5019|4175|4571){0,12}')
     let mastercard = new RegExp('^5[1-5][0-9]{14}$');
     let mastercard2 = new RegExp('^2[2-7][0-9]{14}$');
-
+    let instapayment = new RegExp('^63[7-9]{0,13}')
     let disco1 = new RegExp('^6011[0-9]{12}[0-9]*$');
     let disco2 = new RegExp('^62[24568][0-9]{13}[0-9]*$');
     let disco3 = new RegExp('^6[45][0-9]{14}[0-9]*$');
@@ -14,7 +16,18 @@ export const creditCardType = (cc) => {
     let diners = new RegExp('^3[0689][0-9]{12}[0-9]*$');
     let jcb = new RegExp('^35[0-9]{14}[0-9]*$');
 
-
+    if (instapayment.test(cc)) {
+        return 'instapayment'.toUpperCase();
+    }
+    if (dankort.test(cc)) {
+        return 'dankort'.toUpperCase();
+    }
+    if (dankort.test(cc)) {
+        return 'dankort'.toUpperCase();
+    }
+    if (uatp.test(cc)) {
+        return 'Universal Air Travel Plan'.toUpperCase();
+    }
     if (visa.test(cc)) {
         return 'VISA';
     }
